82721dcde146f5f38a700998007b400c5ba6ec1f
DBC Apprentice <apprentice@devbootcamp.com>
2013-09-30 15:32:12 -0700

    adds validations for user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :teams
   has_many :leagues through :teams
+  validates :username, :email, uniqueness: true
+  validates :firstname, :lastname, :username, :email, :password, presence: true
 end
