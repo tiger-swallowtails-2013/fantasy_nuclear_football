d0aab95fa7e937d269eeaaab3e43bb8c638a5e64
DBC Apprentice <apprentice@devbootcamp.com>
2013-09-30 15:35:01 -0700

    validates presence of name for league and team classes

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,5 @@ class Team < ActiveRecord::Base
   belongs_to :user
   belongs_to :league
   validates :user_id, uniqueness: {scope: :league_id, message: "Each user is allowed only one team per league, you scruffy-looking nerf herder!"}
+  validates :name, presence: true
 end
