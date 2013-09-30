1d3704cd9635a83237282609d22d789ced073bd5
DBC Apprentice <apprentice@devbootcamp.com>
2013-09-30 12:07:38 -0700

    creates table teams: joins users and leagues, defines tables: user and league

--- db/migrate/001_create_table_users.rb
+++ db/migrate/001_create_table_users.rb
@@ -2,8 +2,6 @@ class CreateTableUsers < ActiveRecord::Migration
   def change
     create_table :users do |g|
       g.string :firstname, :lastname, :username, :email, :password
-      g.has_many :teams
-      g.belongs_to :league through :teams
       g.timestamps
     end
   end
--- db/migrate/002_create_table_leagues.rb
+++ db/migrate/002_create_table_leagues.rb
@@ -2,8 +2,6 @@ class CreateTableLeagues < ActiveRecord::Migration
   def change
     create_table :leagues do |g|
       g.string :name
-      g.has_many :teams
-      g.belongs_to :user through :teams
       g.timestamps
     end
   end
