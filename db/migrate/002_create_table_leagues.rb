a958c4eff8261fc125d81bf6aeb8ff7086262b98
DBC Apprentice <apprentice@devbootcamp.com>
2013-09-30 11:55:50 -0700

    generates users and leagues with pertinent fields

--- db/migrate/002_create_table_leagues.rb
+++ db/migrate/002_create_table_leagues.rb
@@ -1,2 +1,10 @@
 class CreateTableLeagues < ActiveRecord::Migration
+  def change
+    create_table :leagues do |g|
+      g.string :name
+      g.has_many :teams
+      g.belongs_to :user through :teams
+      g.timestamps
+    end
+  end
 end
