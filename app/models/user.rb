8fed8706a1d3d30a243a8ff7193fbfdb94020d96
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-30 16:32:07 -0700

    fixes syntax error in associations: has man __, :through __

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :teams
-  has_many :leagues through :teams
+  has_many :leagues, through: :teams
   validates :username, :email, uniqueness: true
   validates :firstname, :lastname, :username, :email, :password, presence: true
 end
