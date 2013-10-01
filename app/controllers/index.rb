1d2b26f35dc768b9ec440bf09290f029d7c6d273
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-10-01 10:20:16 -0700

    new user object created when user logs in through facebook

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,21 +10,26 @@ get '/' do
 end
 
 get '/auth/facebook/callback' do
-  session[:user_id] = User.find_or_create_by(:facebook_id => auth_hash[:uid]).id
-  p session
-
+  session[:user_id] = User.find_or_create_by(prepared_user_data).id
   "Create User with facebook Id of #{auth_hash[:uid]}"
-
-
-
 end
 
 helpers do
+  def prepared_user_data
+    {
+      :facebook_id => auth_hash[:uid],
+      :email => auth_hash[:info][:email],
+      :username => auth_hash[:info][:nickname],
+      :firstname => auth_hash[:info][:first_name],
+      :lastname => auth_hash[:info][:last_name]
+    }
+  end
+
   def auth_hash
     env['omniauth.auth']
   end
 
   def current_user
-    @user = @user || User.find(session[:user_id])
+    @user ||= User.find(session[:user_id])
   end
 end
