8eb32a58bf61bc50e069b0ebfb5bef41c035086e
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-30 20:29:31 -0700

    updates user spec to meet oauth requirements and support

--- spec/associations_spec.rb
+++ spec/associations_spec.rb
@@ -1,18 +1,12 @@
 require_relative './spec_helper'
 
 describe User do
-	it { should validate_presence_of(:firstname) }
-	it { should validate_presence_of(:lastname) }
 	it { should validate_presence_of(:username) }
-	it { should validate_presence_of(:email) }
-	it { should validate_presence_of(:password) }
-	it { should validate_presence_of(:facebook_id) }
 	it { should validate_uniqueness_of(:email) }
 	it { should validate_uniqueness_of(:username) }
 	it { should validate_uniqueness_of(:facebook_id) }
 	it { should have_many(:teams) }
 	it { should have_many(:leagues).through(:teams) }
-	it { should have_many(:politicians).through(:teams)}
 end
 
 describe League do
@@ -25,7 +19,6 @@ describe Team do
 	it { should validate_presence_of(:name) }
 	it { should belong_to(:user) }
 	it { should belong_to(:league) }
-	it { should have_many(:politicians) }
 end
 
 describe Politician do
