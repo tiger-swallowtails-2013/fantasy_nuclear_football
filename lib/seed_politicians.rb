4b467d70c98377a8e87ec7c79feb3436ad9e3a08
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-10-01 18:08:34 -0700

    fills politicians table with data from legislators csv

--- lib/seed_politicians.rb
+++ lib/seed_politicians.rb
@@ -11,6 +11,23 @@ class PoliticiansImporter
 				header = line.map{|item| item.to_sym}
 			else
 				attributes = Hash[header.zip(line)]
+				Politician.create!({
+					first_name: attributes[:firstname],
+					nickname: attributes[:nickname],
+					last_name: attributes[:lastname],
+					middle_name: attributes[:middlename],
+					suffix: attributes[:name_suffix],
+					title: attributes[:title],
+					gender: attributes[:gender],
+					bioguide_id: attributes[:bioguide_id],
+					party: attributes[:party],
+					state: attributes[:state],
+					district: attributes[:district],
+					senate_class: attributes[:senate_class],
+					twitter_id: attributes[:twitter_id],
+					facebook_id: attributes[:facebook_id],
+					birthday: attributes[:birthdate]	
+					})
 			end
 		end
 	end
