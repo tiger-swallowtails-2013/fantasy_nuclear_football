119330c5cfd9ecb1c3b55c448f4dcbc8a0f31d63
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-10-01 18:45:11 -0700

    removes CSV require statement to resolve heroku error

--- lib/seed_politicians.rb
+++ lib/seed_politicians.rb
@@ -1,8 +1,6 @@
 $LOAD_PATH.unshift(File.expand_path('.'))
 require 'config/main'
 
-require 'CSV'
-
 class PoliticiansImporter
 	def self.import(filename = File.dirname(__FILE__) + "/../db/data/legislators.csv")
 		header = nil
