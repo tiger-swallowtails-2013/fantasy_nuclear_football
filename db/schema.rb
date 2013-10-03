7df356730174d4659bcf8297460ff70c2d92f5ab
DBC Apprentice <apprentice@devbootcamp.com>
2013-10-03 00:46:32 -0700

    updates schema after successful migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 6) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "politician_teams", force: true do |t|
+    t.integer "politician_id"
+    t.integer "team_id"
+  end
 
   create_table "politicians", force: true do |t|
     t.string   "first_name"
@@ -29,7 +34,6 @@ ActiveRecord::Schema.define(version: 5) do
     t.string   "title"
     t.string   "chamber"
     t.string   "bioguide_id"
-    t.integer  "team_id"
     t.integer  "district"
     t.integer  "senate_class"
     t.date     "birthday"
