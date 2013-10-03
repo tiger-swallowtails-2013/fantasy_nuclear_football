bf18fe0e0cc4a39711f05174cbc7d5460f77dca2
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-10-03 10:14:25 -0700

    wraps tweetstream process in module so that it can be called from rakefile

--- app/workers/twitter_stream.rb
+++ app/workers/twitter_stream.rb
@@ -2,26 +2,32 @@ require 'tweetstream'
 require_relative '../../config/main'
 require_relative './tweet_store'
 
-TweetStream.configure do |config|
-	config.consumer_key = TWITTER_CONSUMER_KEY
-	config.consumer_secret = TWITTER_CONSUMER_SECRET
-	config.oauth_token = TWITTER_OAUTH_TOKEN
-	config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
-	config.auth_method = :oauth
-end
+module TwitterStream
 
-all_politicians_twitter_handles = Politician.getAllTwitterHandles
-myTweets = TweetStore.new(Politician.getAllTwitterHandles)
+	def start
+		TweetStream.configure do |config|
+			config.consumer_key = TWITTER_CONSUMER_KEY
+			config.consumer_secret = TWITTER_CONSUMER_SECRET
+			config.oauth_token = TWITTER_OAUTH_TOKEN
+			config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
+			config.auth_method = :oauth
+		end
 
-twitter_handles_sample = all_politicians_twitter_handles.sample(400)
-count = 1
+		all_politicians_twitter_handles = Politician.getAllTwitterHandles
+		myTweets = TweetStore.new(Politician.getAllTwitterHandles)
 
-TweetStream::Client.new.track(twitter_handles_sample) do |status|
-	myTweets.push(status.text)
-	if(count >= 10)
 		twitter_handles_sample = all_politicians_twitter_handles.sample(400)
-		myTweets.compile_tweets
-		count = 1;
+		count = 1
+
+		TweetStream::Client.new.track(twitter_handles_sample) do |status|
+			myTweets.push(status.text)
+			if(count >= 10)
+				twitter_handles_sample = all_politicians_twitter_handles.sample(400)
+				myTweets.compile_tweets
+				count = 1;
+			end
+			count+=1
+		end
 	end
-	count+=1
+
 end
