6e2d258da1d25e1fd9d3d3be99c00f321a1ef0a8
Lloyd Nimetz <lnimetz@me.com>
2013-10-03 20:02:27 -0700

    sunlight worker no longer loads repeat data. seeding method also created

--- app/workers/scrape_sunlight.rb
+++ app/workers/scrape_sunlight.rb
@@ -8,12 +8,28 @@ Congress.key = 'b4a075b0ee6b4978835984db7173a173'
 module SunlightScraper
 
   def self.scrape
-    events = Congress.floor_updates[:results].select {|update| update[:legislator_ids] != []}
-    events.select do |update|
-      update_time = DateTime.strptime(update[:timestamp], '%Y-%m-%dT%H:%M:%S%z')
-      ((DateTime.now - update_time) * 24).to_f < 1
-    end 
+    events = get_legislator_events
+    remove_old_events(events,1)
+    add_to_database(events)
+  end
 
+  def self.seed
+    events = get_legislator_events
+    add_to_database(events)
+  end
+
+  def self.remove_old_events(events, hours)
+    events.select! do |update|
+      update_time = DateTime.strptime(update[:timestamp], '%Y-%m-%dT%H:%M:%S%z')
+      ((DateTime.now - update_time) * 24).to_f < hours
+    end 
+  end
+
+  def self.get_legislator_events
+    Congress.floor_updates[:results].select {|update| update[:legislator_ids] != []}
+  end
+
+  def self.add_to_database(events)
     events.each do |pol|
       SunlightScore.create(activity: 1, bioguide_id: pol[:legislator_ids][0],
                         sunlight_timestamp: pol[:timestamp], description: pol[:update])
