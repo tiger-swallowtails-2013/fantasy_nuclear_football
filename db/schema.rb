c4a3a906e76f2b33d4da13ab7fb0e977412d30cf
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-10-03 23:28:44 -0700

    basic page elements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,13 +34,13 @@ ActiveRecord::Schema.define(version: 7) do
     t.string   "title"
     t.string   "chamber"
     t.string   "bioguide_id"
-    t.string   "bio"
     t.integer  "district"
     t.integer  "senate_class"
     t.boolean  "in_office"
     t.date     "birthday"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "bio"
   end
 
   create_table "scores", force: true do |t|
