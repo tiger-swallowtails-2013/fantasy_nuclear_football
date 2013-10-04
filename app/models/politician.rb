2761c57d49c1c712c4308da0c007cde8931efda9
Apprentice <apprentice@devbootcamp.com>
2013-10-03 22:00:45 -0700

    politician can now calculate individual scores, total score

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -11,13 +11,26 @@ class Politician < ActiveRecord::Base
   	all.delete_if{ |politician| politician.in_office == false || politician.twitter_id.nil? }.map{ |politician| politician.twitter_id }.map!{|politician| "@#{politician}"}
   end
 
-  def total_score(week)
+  def twitter_score(week)
     pol_score = scores.where(game_number: week)[0]
-    if pol_score
-      (pol_score.twitter_mentions / 20) + (pol_score.vote_score * 5)
+    if pol_score && pol_score.twitter_mentions
+      pol_score.twitter_mentions.to_f / 20
     else
       0
     end
+  end
+
+  def sunlight_score(week)
+    pol_score = scores.where(game_number: week)[0]
+    if pol_score && pol_score.vote_score
+      pol_score.vote_score * 5
+    else
+      0
+    end
+  end
+
+  def total_score(week)
+    twitter_score(week) + sunlight_score(week)
   end
 
   def self.top_scorers(week, limit = 10)
