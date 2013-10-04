07af6de8cec2ab1f7280ca92c6ee37dee0ab557d
Lloyd Nimetz <lnimetz@me.com>
2013-10-03 18:17:12 -0700

    dinnner

--- app/workers/scrape_sunlight.rb
+++ app/workers/scrape_sunlight.rb
@@ -1,12 +1,18 @@
 require 'congress'
 require_relative '../../config/main.rb'
 
-Congress.key = ENV['SUNLIGHT_LEGISLATORS_KEY']
+# Congress.key = ENV['SUNLIGHT_LEGISLATORS_KEY']
+Congress.key = 'b4a075b0ee6b4978835984db7173a173'
+
 
 module SunlightScraper
 
   def self.scrape
     events = Congress.floor_updates[:results].select {|update| update[:legislator_ids] != []}
+    events.select do |update|
+      update_time = DateTime.strptime(update[:timestamp], '%Y-%m-%dT%H:%M:%S%z')
+      ((DateTime.now - update_time) * 24).to_f < 1
+    end 
 
     events.each do |pol|
       SunlightScore.create(activity: 1, bioguide_id: pol[:legislator_ids][0],
--- config/main.rb
+++ config/main.rb
@@ -36,5 +36,5 @@ end
 
 require 'redis'
 require 'uri'
-uri = URI.parse(ENV['REDISTOGO_URL'])
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# uri = URI.parse(ENV['REDISTOGO_URL'])
+# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
